96cf2b3ccc6b3adee7d8833aab91fa04b835c8c7
im-o <frizgt@gmail.com>
2020-08-10 02:29:22 +0800

    :zap: show data on view from viewmodel

--- app/src/main/java/com/stimednp/kotlinsamplemvvm/view/MainActivity.kt
+++ app/src/main/java/com/stimednp/kotlinsamplemvvm/view/MainActivity.kt
@@ -1,12 +1,29 @@
-package com.stimednp.kotlinsamplemvvm
+package com.stimednp.kotlinsamplemvvm.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.stimednp.kotlinsamplemvvm.databinding.ActivityMainBinding
+import com.stimednp.kotlinsamplemvvm.viewmodel.MovieViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var movieViewModel: MovieViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        movieViewModel = ViewModelProvider(this)[MovieViewModel::class.java]
+        movieViewModel.getMovies().observe(this, Observer {
+            Log.e("INIIII","DATTTTAAA -> ${it.size}")
+        })
     }
 }
